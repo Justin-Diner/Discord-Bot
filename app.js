cde9d1a67c01ca5e5ab166f5d8e9b88f42c30082
Justin <90215009+DinerJ@users.noreply.github.com>
2021-10-03 22:46:22 -0400

    Working on Cleaning Up Code

--- app.js
+++ app.js
@@ -1,42 +1,34 @@
-const Discord = require("discord.js");
-const fetch = require("node-fetch");
-const config = require("./data/config.json");
-const intents = new Discord.Intents(32767);
-const client = new Discord.Client({ intents });
+const Discord = require('discord.js')
+const fetch = require('node-fetch')
 const axios = require('axios');
+const config = require('./data/config.json')
+
+const intents = new Discord.Intents(32767)
+const client = new Discord.Client({ intents })
 const command = require('./command');
-
-// Logging into my Disord Bot 
+// Logging into my Disord Bot
 client.login(config.token);
-
-//Bot is Online Notice when using Node.js
-client.on("ready", () => {
-    console.log('Bot is online')
+// Bot is Online Notice when using Node.js
+client.on('ready', () => {
+  console.log('Bot is online')
 })
-
-//Sad Words for Motivational Bot
-const sadWords = ["sad", "depressed", "unhappy", "angry"];
-const encouragements = [
-    "Cheer up!",
-    "Keep Going!",
-    "You rock!"
-];
-
+// Sad Words for Motivational Bot
+const sadWords = ['sad', 'depressed', 'unhappy', 'angry']
+const encouragements = ['Cheer up!', 'Keep Going!', 'You rock!']
 // Ping and Pong Response 
-client.on("messageCreate", msg => {
-    if (msg.content === "ping") {
-        msg.reply("pong")
-    }
+client.on('messageCreate', msg => {
+  if (msg.content === 'ping') {
+    msg.reply('pong')
+  }
 })
-
 // Returning quotes on the Motivational Bot (Connecting to Zenquotes API)
 function getQuote() {
-    return fetch("https://zenquotes.io/api/random")
+  return fetch('https://zenquotes.io/api/random')
     .then(res => {
-        return res.json()
+      return res.json()
     })
     .then(data => {
-        return data[0]["q"]+ " - " + data[0]["a"]
+      return data[0]['q']+ ' - ' + data[0]['a']
     })
 }
 
