7fe04db48d3dc33bed40a431da2a78b3c51c5db8
Justin <90215009+DinerJ@users.noreply.github.com>
2021-10-18 22:40:44 -0400

    Newly Added Commands

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
-const Discord = require('discord.js')
-const fetch = require('node-fetch')
-const axios = require('axios')
-const config = require('./data/config.json')
+import fetch from 'node-fetch'
+import Discord, { Message } from 'discord.js'
+import axios from 'axios'
+
+import { config } from './data/config.js'
 
 const intents = new Discord.Intents(32767)
 const client = new Discord.Client({ intents })
+const prefix = '#';
 
 // Logging into my Disord Bot
 client.login(config.token)
@@ -17,7 +19,7 @@ const sadWords = ['sad', 'depressed', 'unhappy', 'angry']
 const encouragements = ['Cheer up!', 'Keep Going!', 'You rock!']
 // Ping and Pong Response
 client.on('messageCreate', msg => {
-  if (msg.content === 'ping') {
+  if (msg.content === prefix + 'ping') {
     msg.reply('pong')
   }
 })
@@ -33,8 +35,10 @@ function getQuote() {
 }
 // Messages Code
 client.on('messageCreate', msg => {
+  let args = msg.content.substring(prefix.length).split(' ');
+// Inpsire
   if (msg.author.bot) return
-  if (msg.content === '$inspire') {
+  if (msg.content === prefix + 'inspire') {
     getQuote().then(quote => msg.channel.send(quote))
   }
   if (sadWords.some(word => msg.content.includes(word))) {
@@ -42,25 +46,74 @@ client.on('messageCreate', msg => {
       encouragements[Math.floor(Math.random() * encouragements.length)]
     msg.reply(encouragement)
   }
-  if (msg.content.includes('#weather')) {
-    const args = msg.content
+// Rock, Paper, Scissors
+  if (msg.content.includes(prefix + 'rps')) {
+    if (!args[1]) {
+      return msg.channel.send('Please inlcude your choice.')
+    }
+    let choices = ['rock', 'paper', 'scissors'];
+    if (choices.includes((args[1]).toLowerCase())) {
+      let number = Math.floor(Math.random() * 3);
+      if (number == 1) {
+        return msg.channel.send('TIE! We both had ' + (args[1]).toLocaleLowerCase() + '!');
+      }
+      if (number == 2) {
+        if ((args[1]).toLowerCase() == 'rock') {
+          return msg.channel.send ('You LOSE! I had paper.')
+        }
+        if ((args[1]).toLowerCase() == 'scissors') {
+          return msg.channel.send ('You LOSE! I had rock.')
+        }
+        if ((args[1]).toLowerCase() == 'paper') {
+          return msg.channel.send ('You LOSE! I had scissors.')
+        }
+      }
+      if (number == 0) {
+        if ((args[1]).toLowerCase() == 'rock') {
+          return msg.channel.send ('You WON! I had scissors.')
+        }
+        if ((args[1]).toLowerCase() == 'scissors') {
+          return msg.channel.send ('You WON! I had paper.')
+        }
+        if ((args[1]).toLowerCase() == 'paper') {
+          return msg.channel.send ('You WON! I had rock.')
+        }
+      }
+    }
+  }
+// Weather Pull
+  if (msg.content.includes(prefix + 'weather')) {
+    if (!args[1]) {
+      return msg.channel.send('Please enter the command again and include a city.');
+    }
+    const city = msg.content
       .slice(9)
       .trim()
       .split(/ +/g)
-    const command = args.join('').toLowerCase()
+    const command = city.join(' ').toLowerCase()
     const commandString = JSON.stringify(command)
     getWeather(commandString).then(temp => msg.channel.send(temp))
   }
+// Coinflip 
+if (msg.content.includes(prefix + 'coinflip')) {
+  let number = Math.floor(Math.random() * 2);
+  if (number == 1) {
+    msg.channel.send('Heads!');
+  } else (msg.channel.send('Tails!')); 
+}
 })
-// Example Link = https://api.openweathermap.org/data/2.5/weather?q=miami&units=imperial&appid=0eed50cd5ca81be23ba0c10b6d5a9373"
+// Weather Function 
 async function getWeather(arg1) {
+  // Removing Quotes
   const cityName = arg1.replace(/["]+/g, '')
-  const cityNameCaps = cityName.split('')
+  // Capitalizing Each Word for City Titles
+  const cityNameCaps = cityName.split(' ')
   for (let i = 0; i < cityNameCaps.length; i += 1) {
     cityNameCaps[i] =
       cityNameCaps[i][0].toUpperCase() + cityNameCaps[i].substr(1)
   }
   const cityNameCapsRem = cityNameCaps.join(' ')
+  // Concating API Link for API Request
   const weatherLink = 'https://api.openweathermap.org/data/2.5/weather?q='
   const weatherUnits = '&units=imperial'
   const weatherApiKey = '&appid=0eed50cd5ca81be23ba0c10b6d5a9373'
@@ -68,6 +121,6 @@ async function getWeather(arg1) {
   const response = await fetch(weatherLinkFull)
   const data = await response.json()
   return (
-    'It is ' + data['main']['temp'] + ' degrees in ' + cityNameCapsRem + '.'
+    'It is ' + data.main.temp + ' degrees in ' + cityNameCapsRem + '.'
   )
 }
---
+++ data/config.js
@@ -0,0 +1,6 @@
+export const config = {
+    token: 'ODkxNzAxOTY1NTY0ODA5MjU3.YVCMKw.3sFCnZcGXezdPhGlrg73FbbHagQ',
+    prefix: '!',
+    weatherApiKey: '0eed50cd5ca81be23ba0c10b6d5a9373',
+    alphaVantageApiKey: '3OTLKGXZI6VXLSMC'
+  }
